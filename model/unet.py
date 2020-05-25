6da699445294ebd10de5d9e13bbaddecf43858d5
shupei <freddiechang1994@live.com>
2020-05-25 11:07:37 -0600

    test

--- model/unet.py
+++ model/unet.py
@@ -26,6 +26,7 @@ class UNet(nn.Module):
         self.outc = OutConv(64, n_classes)
 
     def forward(self, x):
+        """
         i1 = x.max(dim=1)[0].unsqueeze(1)
         i1 = (i1 - self.hdr_illu_target).abs() + self.illu_offset
         x1 = self.inc(x) * i1
@@ -42,4 +43,19 @@ class UNet(nn.Module):
         x = self.up3(x, x2) * i2
         x = self.up4(x, x1) * i1
         logits = self.outc(x) * i1
+        return logits
+        """
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+
+        x3 = self.down2(x2)
+
+        x4 = self.down3(x3)
+
+        x5 = self.down4(x4)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        logits = self.outc(x)
         return logits
