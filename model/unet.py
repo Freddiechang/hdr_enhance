0dcd61b833d03f6a57af30a8c730dc2d9dc70c97
shupei <freddiechang1994@live.com>
2020-08-13 14:23:32 -0600

    momory limit

--- model/unet.py
+++ model/unet.py
@@ -22,10 +22,9 @@ class UNet(nn.Module):
         self.up1 = Up(1024, 512 // factor, bilinear)
         self.up2 = Up(512, 256 // factor, bilinear)
         self.up3 = Up(256, 128 // factor, bilinear)
-        self.attn1 = SelfAttn(128 // factor, '')
         self.up4 = Up(128, 64, bilinear)
-        self.attn2 = SelfAttn(64, '')
         self.outc = OutConv(64, n_classes)
+        self.attn = SelfAttn(256 // factor, '')
 
     def forward(self, x):
         """
@@ -49,17 +48,13 @@ class UNet(nn.Module):
         """
         x1 = self.inc(x)
         x2 = self.down1(x1)
-
         x3 = self.down2(x2)
-
         x4 = self.down3(x3)
-
         x5 = self.down4(x4)
         x = self.up1(x5, x4)
         x = self.up2(x, x3)
+        x, _ = self.attn(x)
         x = self.up3(x, x2)
-        x, _ = self.attn1(x)
         x = self.up4(x, x1)
-        x, _ = self.attn2(x)
         x = self.outc(x)
         return x
